d5105105577f6b9f72ef8806887c0e1fba52c692
Emil Geredy <mysticman@users.noreply.github.com>
2025-04-01 21:11:22 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,1 @@
+const OPENAI_API_KEY = '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';
